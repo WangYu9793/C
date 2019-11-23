1503633430a5fa73342d2314c81e28f22b46d671
WangYu9793 <1141288616@qq.com>
2019-11-23 20:02:52 +0800

    test

---
+++ 201911231/201911231/test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<Windows.h>
+int f(int a)
+{
+	return a % 2;
+}
+int main()
+{
+	int s[8] = { 1, 3, 5, 2, 4, 6 }, i, d = 0;
+	for (i = 0; f(s[i]); i++)
+	{ d += s[i]; }
+	printf("%d\n", d);
+	system("pause");
+	return 0;
+}
