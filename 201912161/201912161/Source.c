9689774f9e62e79e3c2689ddb8e0698d88fb4958
WangYu9793 <1141288616@qq.com>
2019-12-16 22:52:57 +0800

    test

---
+++ 201912161/201912161/Source.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a = 0, i;
+	for (i = 1; i<5; i++)
+	{
+		switch (i)
+		{
+		case 0:
+		case 3:a += 2;
+		case 1:
+		case 2:a += 3;
+		default:a += 5;
+		}
+	}
+	printf("%d\n", a);
+	system("pause");
+	return 0;
+}
