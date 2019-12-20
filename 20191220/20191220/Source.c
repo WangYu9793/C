443f71b6415ac0a0101e3f6a281fc19b086daab5
WangYu9793 <1141288616@qq.com>
2019-12-20 17:29:52 +0800

    test

---
+++ 20191220/20191220/Source.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int f(int a)
+{
+	int b = 0;
+	static int c = 3;
+	a = c++, b++;
+	return (a);
+}
+int main()
+{
+	int a = 2, i, k;
+	for (i = 0; i < 2; i++)
+		k = f(a++);
+	printf(" % d\n", k);
+	system("pause");
+	return 0;
+}
