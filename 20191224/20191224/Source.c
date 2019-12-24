0bb8961bb8821913b56a7a6c91bffab070c658d4
WangYu9793 <1141288616@qq.com>
2019-12-24 21:18:27 +0800

    test

---
+++ 20191224/20191224/Source.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>   
+int main(){ 
+	int i, j, k;
+  for(i=1;i<=3;++i){
+	for(j=1;j<=3;++j)
+	{
+		for(k=1;k<=3;++k){
+			if (i==j||j==k||i==k)
+			{
+				continue;
+			}
+			if(i!=1&&k!=1&&k!=3)
+			{
+				printf("Avs%c\n",i+'X'-1);      
+				printf("Bvs%c\n",j+'X'-1);
+				printf("Cvs%c\n",k+'X'-1);      
+				return  0;
+			}
+		}
+	}
+}
+return  0;
+}
