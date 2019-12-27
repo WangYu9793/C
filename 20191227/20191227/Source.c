cab87225abee7c0bbdda920d7ec50c57f968691a
WangYu9793 <1141288616@qq.com>
2019-12-27 20:27:22 +0800

    test

---
+++ 20191227/20191227/Source.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+void convert(long n) {
+	long i;
+	if ((i = n / 10) != 0)    
+		convert(i);
+	putchar(n % 10 + '0');
+}
+int main()
+{
+	long number;
+	scanf("%ld", &number);
+	if (number<0)
+	{
+		putchar('-'); number = -number;
+	} 
+	convert (number);
+}
