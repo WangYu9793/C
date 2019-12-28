578166c1d432170499343666ceb54b9b0e2a955f
WangYu9793 <1141288616@qq.com>
2019-12-28 18:49:58 +0800

    test

---
+++ 20191228/20191228/Source.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h> 
+#include"conio.h"
+int removeElement(int array[], int numsSize, int val){
+	int count = 1;
+	int cur = 0;
+	while (cur < numsSize){
+		if (array[cur] != val)
+			array[cur - count] = array[cur];
+		else count++;
+	}
+	return numsSize - count;
+}
+int main(){
+	int a[] = { 0, 1, 2, 2, 3, 0, 4, 2 };
+	int val = 2;
+	int num = sizeof(a) / sizeof(a[0]);
+	printf("%d",removeElement(a, num, val));
+	system("pause");
+	return 0;
+}
